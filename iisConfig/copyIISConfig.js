970e0f81d2c8709c353e0ba6bd5aa81b411a69ce
Srinivasan Sekar <poonguzhali@r3htechnologies.com>
2022-12-31 13:08:13 +0530

    Web config modified

---
+++ iisConfig/copyIISConfig.js
@@ -0,0 +1,9 @@
+const fs = require('fs-extra');
+
+const webConfigPath = './dist/web.config';
+
+if (fs.existsSync(webConfigPath)) {
+	fs.unlinkSync(webConfigPath);
+}
+
+fs.copySync('iisConfig/web.config', webConfigPath);
